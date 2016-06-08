682a36d1be540d09d56a271deaa18e41d9f559db
ren <luoh2@rpi.edu>
2016-06-07 20:31:15 -0400

    finished writing signup page with form. Also wrote integration test for failed user signup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    #will open debug console in rails server terminal. Shows variable values
+    #debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # handle successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
