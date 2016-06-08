efe83956cdde6e33ef5fdaf5f617acb9755e1e2e
ren <luoh2@rpi.edu>
2016-06-08 19:10:01 -0400

    persistent session using cookies: login & logout.

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -8,10 +8,34 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  attr_accessor :remember_token
+
   # returns the hash digest of given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string,cost: cost)
+  end
+
+  # returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end  
+
+  # remembers a suer in the databse for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true iff the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest,nil)
   end
 end
