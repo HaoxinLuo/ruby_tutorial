4ba79dab870e610d40ff7bf258e28fe19a13b43c
ren <luoh2@rpi.edu>
2016-06-13 21:29:43 -0400

    Added friendly forwarding

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -48,4 +48,15 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+
+  # redirects to a stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # stores the url trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
