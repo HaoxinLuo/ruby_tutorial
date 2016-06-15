1cb631872bfeca9bf4d2c6c22436579f2ca90e1b
ren <luoh2@rpi.edu>
2016-06-15 12:04:24 -0400

    created account activation model

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email 
+  before_create :create_activation_digest
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -8,7 +12,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  attr_accessor :remember_token
 
   # returns the hash digest of given string
   def User.digest(string)
@@ -37,5 +40,16 @@ class User < ActiveRecord::Base
   # forgets a user
   def forget
     update_attribute(:remember_digest,nil)
+  end
+
+  private
+  
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
